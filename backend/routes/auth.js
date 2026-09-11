6f634341210b80553e2fed05c5e7d51eab9f09b3
Henrique gustavo de souza <henriquerenan.gustavoo@gmail.com>
2026-09-11 08:38:38 -0300

    add .ds

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, cadastro } = require('../controllers/authController');
-
-router.post('/login', login);
-router.post('/cadastro', cadastro);
-
-module.exports = router;
